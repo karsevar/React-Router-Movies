cd7dde1dbbd9e4dc6f35098bc32f75b2bcb39177
masonkarsevar <masonkarsevar@gmail.com>
2019-07-22 12:17:26 -0700

    basic dynamic router functionality finished. Will need have Movie component receive match.params

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Route} from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -13,8 +14,10 @@ const App = () => {
 
   return (
     <div>
-      <Route exact path='/' component={() => <SavedList list={savedList} />} />
-      <MovieList />
+      <SavedList list={savedList} />
+      {/*<Route exact path='/' component={() => <SavedList list={savedList} />} />*/}
+      <Route exact path='/' component={MovieList} />
+      <Route path='/movie/:id' component={Movie} />
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
 
 const MovieList = props => {
@@ -22,7 +23,9 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/Movie/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
